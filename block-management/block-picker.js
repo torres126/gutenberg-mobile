65e4e602b59d595921be071ae403155a7c0612f0
Maxime Biais <maxime.biais@gmail.com>
2018-04-19 11:51:17 +0200

    Use a FlatList to show a pseudo-grid-view with some default blocks

--- block-management/block-picker.js
+++ block-management/block-picker.js
@@ -2,12 +2,46 @@
  * @format */
 
 import React from 'react';
-import { Modal, Text, TouchableHighlight, View } from 'react-native';
+import { Modal, Text, TouchableNativeFeedback, FlatList, View, StyleSheet } from 'react-native';
 
 type PropsType = { visible: boolean };
 type StateType = {};
 
 export default class BlockPicker extends React.Component<PropsType, StateType> {
+	renderItem( { item } ) {
+		return (
+			<TouchableNativeFeedback onPress={ () => {} }>
+				<View
+					style={ {
+						flex: 1,
+						margin: 4,
+						height: 150,
+						justifyContent: 'center',
+						backgroundColor: '#CCC',
+					} }
+				>
+					<Text style={ { textAlign: 'center' } }>{ item.text } </Text>
+				</View>
+			</TouchableNativeFeedback>
+		);
+	}
+
+	renderList() {
+		return (
+			<FlatList
+				contentContainerStyle={ styles.list }
+				data={ [
+					{ key: '1', text: 'Paragraph' },
+					{ key: '2', text: 'Code' },
+					{ key: '3', text: 'More' },
+				] }
+				horizontal={ false }
+				numColumns={ 2 }
+				renderItem={ this.renderItem }
+			/>
+		);
+	}
+
 	render() {
 		return (
 			<View>
@@ -19,15 +53,17 @@ export default class BlockPicker extends React.Component<PropsType, StateType> {
 					onRequestClose={ () => {} }
 				>
 					<View>
-						<View>
-							<Text>Hey Ponies!</Text>
-							<TouchableHighlight onPress={ () => {} }>
-								<Text>Hide Modal</Text>
-							</TouchableHighlight>
-						</View>
+						<View>{ this.renderList() }</View>
 					</View>
 				</Modal>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create( {
+	list: {
+		justifyContent: 'center',
+		margin: 4,
+	},
+} );
