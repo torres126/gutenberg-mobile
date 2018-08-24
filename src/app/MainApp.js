f6bd57b465740bcb45b5afe0cd751235c614a687
Jorge Bernal <jbernal@gmail.com>
2018-08-24 13:19:51 +0200

    Load demo HTML from file

--- src/app/MainApp.js
+++ src/app/MainApp.js
@@ -2,6 +2,7 @@
  * @format */
 
 import React from 'react';
+import { Image } from 'react-native';
 
 import BlockManager, { type BlockListType } from '../block-management/block-manager';
 
@@ -10,6 +11,18 @@ type StateType = {};
 
 export default class MainScreen extends React.Component<PropsType, StateType> {
 	render() {
+		const { parseBlocksAction } = this.props;
+		const demo = require( '../../assets/demo.html' );
+		const demoAsset = Image.resolveAssetSource( demo );
+		if ( demoAsset.uri ) {
+			fetch( demoAsset.uri )
+				.then( (response) => response.text() )
+				.catch( (error) => console.error( 'Error:', error ) )
+				.then( (html) => {
+					parseBlocksAction( html );
+				} );
+		}
+
 		return <BlockManager { ...this.props } />;
 	}
 }
